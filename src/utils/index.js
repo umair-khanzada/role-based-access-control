b8569070ffa7f34187f466a8962d749a39b59a06
umair-khanzada <umairkz52@gmail.com>
2020-07-16 01:51:00 +0500

    create utility function for filtering allowed routes.

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,14 @@
+import { intersection } from 'lodash';
+
 export function isArrayWithLength(arr) {
 	return (Array.isArray(arr) && arr.length)
+}
+
+export function getAllowedRoutes(routes) {
+	const roles = JSON.parse(localStorage.getItem('roles'));
+	return routes.filter(({ permission }) => {
+		if(!permission) return true;
+		else if(!isArrayWithLength(permission)) return true;
+		else return intersection(permission, roles).length;
+	});
 }
