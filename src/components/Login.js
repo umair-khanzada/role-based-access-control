8cf715e4a23723f2bfb492ad2f6e87728250e413
umair-khanzada <umairkz52@gmail.com>
2020-07-15 19:50:07 +0500

    refactor login component.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,70 +1,66 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { Alert, Form, Button, Row, Col } from 'react-bootstrap';
 import { JumbotronWrapper } from './common';
 import { Link } from 'react-router-dom';
-import { Roles } from 'config';
-import history from 'util/history';
+import { useHistory } from "react-router-dom";
 
-class Login extends Component {
-	state = { selected: [] };
+function Login() {
+	let [selected, setSelected] = useState([]);
+	let history = useHistory();
 
-	handleChange = (e) => {
+	function handleChange(e) {
+		const { options } = e.target;
 		let selected = [];
-		for (let i = 0; i < e.target.options.length; i++) {
-			if (e.target.options[i].selected) {
-				selected.push(e.target.options[i].value);
+
+		for (let i = 0; i < options.length; i++) {
+			if (options[i].selected) {
+				selected.push(options[i].value);
 			}
 		}
-		this.setState({ selected });
-	};
-
-	handleClick = () => {
-		localStorage.setItem('roles', JSON.stringify(this.state.selected));
-		history.push('/app');
-	};
-
-	render() {
-		return (
-			<JumbotronWrapper title="Login" description="">
-				<Form.Group controlId="exampleForm.ControlSelect1">
-					<Form.Label>Select Role:</Form.Label>
-					<Form.Control
-						as="select"
-						value={this.state.selected}
-						onChange={this.handleChange}
-						multiple
-					>
-						<option value="SUPER_ADMIN">SUPER_ADMIN</option>
-						<option value="ADMIN">ADMIN</option>
-						<option value="MANAGER">MANAGER</option>
-						<option value="CUSTOMER">CUSTOMER</option>
-						<option value="GUEST">GUEST</option>
-					</Form.Control>
-				</Form.Group>
-				<Alert variant="primary">Support multi roles.</Alert>
-				<Row className="text-right">
-					<Col sm={11}>
-						<Link to="/forgot-password">Forgot Password</Link>
-					</Col>
-					<Col sm={1}>
-						<Link to="/register">Register</Link>
-					</Col>
-				</Row>
-				<Button
-					variant="primary"
-					onClick={this.handleClick}
-					disabled={!this.state.selected.length}
-				>
-					Login
-				</Button>
-			</JumbotronWrapper>
-		);
+		setSelected(selected);
 	}
+
+	function handleClick() {
+		localStorage.setItem('roles', JSON.stringify(selected));
+		history.push('/app');
+	}
+
+	return (
+		<JumbotronWrapper title="Login" description="">
+			<Form.Group controlId="exampleForm.ControlSelect1">
+				<Form.Label>Select Role:</Form.Label>
+				<Form.Control
+					as="select"
+					value={selected}
+					onChange={handleChange}
+					multiple
+				>
+					{/* TODO: Dynamic role options */}
+					<option value="SUPER_ADMIN">SUPER_ADMIN</option>
+					<option value="ADMIN">ADMIN</option>
+					<option value="MANAGER">MANAGER</option>
+					<option value="CUSTOMER">CUSTOMER</option>
+					<option value="GUEST">GUEST</option>
+				</Form.Control>
+			</Form.Group>
+			<Alert variant="primary">Support multi roles.</Alert>
+			<Row className="text-right">
+				<Col sm={11}>
+					<Link to="/forgot-password">Forgot Password</Link>
+				</Col>
+				<Col sm={1}>
+					<Link to="/register">Register</Link>
+				</Col>
+			</Row>
+			<Button
+				variant="primary"
+				onClick={handleClick}
+				disabled={!selected.length}
+			>
+				Login
+			</Button>
+		</JumbotronWrapper>
+	);
 }
-
-Login.propTypes = {};
-
-Login.defaultProps = {};
 
 export default Login;
