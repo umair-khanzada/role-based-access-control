b1e0b4976321d0df33d3a43308922408a5f986a4
umair-khanzada <umairkz52@gmail.com>
2020-07-15 19:23:54 +0500

    update route config structure.

--- src/routes/routeConfig.js
+++ src/routes/routeConfig.js
@@ -1,11 +1,17 @@
 import { Roles } from 'config'
 import { Users, Orders, Products } from 'components';
 
+// TODO:
+/*
+* 1. Make title optional
+* 2. Make title multi type support ie: (string, node, react element)
+* 3. Add child route support
+* */
 // when omit the permission or permission is a empty array it means all authenticated users/roles have access to that route
 export default [
 	{
 		component: Users,
-		url: '/users',
+		path: '/users',
 		title: 'Users',
 		permission: [
 			Roles.SUPER_ADMIN,
@@ -16,17 +22,24 @@ export default [
 	},
 	{
 		component: Orders,
-		url: '/orders',
+		path: '/orders',
 		title: 'Orders',
 		permission: [
 			Roles.SUPER_ADMIN,
 			Roles.ADMIN,
 			Roles.MANAGER
+		],
+		children: [
+			{
+				component: Products,
+				path: '/:id',
+				title: 'Orders Details',
+			}
 		]
 	},
 	{
 		component: Products,
-		url: '/',
+		path: '/',
 		title: 'Products',
 	}
 ]
