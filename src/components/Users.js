4f42d6b86cb5ce43455f97157b16110232bebe1a
umair-khanzada <umairkz52@gmail.com>
2020-07-25 02:35:06 +0500

    add faker in populate users list from faker.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,9 +1,21 @@
 import React, { memo } from 'react';
-import { JumbotronWrapper } from './common';
+import { users } from '__mock__';
 
 function Users() {
 	return (
-		<JumbotronWrapper title="Users" />
+		<ul className="w3-ul w3-card-4" style={{background: '#fff', margin: '100px 0'}}>
+			{users.map(user => (
+				<li className="w3-bar">
+					<img src={user.avatar} className="w3-bar-item w3-circle w3-hide-small" style={{width: '85px'}} alt="user_avatar"/>
+					<div className="w3-bar-item">
+						<span className="w3-large">{user.firstName} {user.lastName}</span>
+						<br />
+						<span>{user.email}</span>
+					</div>
+				</li>
+			))}
+
+		</ul>
 	)
 }
 
